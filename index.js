54713755643442ac42331b6a63116354ae829c6c
Mbibhuprasad <128397345+Mbibhuprasad@users.noreply.github.com>
2023-12-07 15:19:20 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+let mail=document.getElementById("mail");
+let error=document.getElementById("error");
+let submit=document.getElementById("submit");
+let form=document.getElementById("form");
+
+
+submit.addEventListener('click',(e)=>{
+    if(!mail.value.match(/^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+.+[com]+$/)){
+        error.style.display="block";
+        mail.style.borderColor="hsl(4, 100%, 67%)";
+        mail.addEventListener('click',function (e){
+            error.style.display="none";
+            mailBox.style.borderColor="grey";
+        })
+    }else{
+        error.style.display="none";
+        mail.style.borderColor="grey";
+    }
+})
